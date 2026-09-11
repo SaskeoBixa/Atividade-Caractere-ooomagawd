74dc5ae29a00b6cf6f1dd6119cac57129c1d0277
DESKTOP-4A7G64Q\User <user@etec.com>
2026-09-11 19:22:35 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("+--- Primeiro Caractere ---+");
+Console.Write("|Digite algo....:");
+
+string txtdigitado = Console.ReadLine()!;
+
+Console.WriteLine(txtdigitado.Trim().Substring(0,1));
